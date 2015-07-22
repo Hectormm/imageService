cb7ad45c6dad99ca72284f63b09c63a4b2b26604
Hectormm <hectormm@gmail.com>
2015-07-22 16:34:35 +0200

    directorio public duplicado

--- app.js
+++ app.js
@@ -19,10 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
-
-app.use(express. static(__dirname + '/public' ));       //Directorio publico
 app.set('port', process.env.PORT || 5000);          //Set puerto
 
 
